1f4ad3cac82c061d5d1859ebdfff1ed944a6c20c
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-05-30 16:10:59 +0300

    Add hover effect to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import ButtonProps from "./ButtonTypes";
 
 function Button(props: ButtonProps) {
     const { type, link, text, centered, long, onClick } = props;
-    const core = <button type={type} onClick={onClick} className={`${centered ? 'mx-auto' : ''} ${long ? 'w-[240px]' : ''} min-w-[178px] border-0 flex justify-center items-center py-[14px] bg-purple-80 rounded-full font-normal text-white text-[16px]/[24px] md:text-[16px]/[24px] xl:text-[16px]/[24px]`}>{text}</button>;
+    const core = <button type={type} onClick={onClick} className={`${centered ? 'mx-auto' : ''} ${long ? 'w-[240px]' : ''} min-w-[178px] border-0 flex justify-center items-center py-[14px] bg-purple-80 hover:bg-purple-30 transition-all rounded-full font-normal text-white text-[16px]/[24px] md:text-[16px]/[24px] xl:text-[16px]/[24px]`}>{text}</button>;
     return link ? (<Link to={link.target} smooth={true} duration={500}>{core}</Link>) : (core);
  }
 
