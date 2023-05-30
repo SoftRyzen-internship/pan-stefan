2772fcbaf956707d0624e9518d7246ef44d0c43a
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-05-30 18:08:09 +0300

    Create OrderButton component

---
+++ src/components/OrderButton/OrderButton.tsx
@@ -0,0 +1,5 @@
+function OrderButton() { 
+    return (<button type='button' className='fixed top-[10%] md:top-[12%] xl:top-[13%] right-[6%] md:right-[4%] xl:right-[2%] w-[73px] md:w-[127px] h-[73px] md:h-[127px] flex justify-center items-center border border-purple-80 hover:border-purple-30 bg-purple-80 hover:bg-lightWite ring-[1px] ring-offset-[3px] ring-purple-80 hover:ring-purple-30 transition-all rounded-full text-white hover:text-purple-80 text-[12px]/[28px] md:text-[18px]/[28px]'>Замовити</button>);
+}
+
+export default OrderButton;
