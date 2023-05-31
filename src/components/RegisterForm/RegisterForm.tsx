52292b4e58911ddb55613038736db04371a0a6c8
Ann Sarviro <annka_nuha@ai.ua>
2023-05-31 15:36:39 +0300

    update: change telegram respons method

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -3,7 +3,7 @@ import FormInput from '../FormInput/FormInput';
 import formField from './formFields';
 import Idata from './Idata';
 import fieldsParams from './fieldsParams';
-import axios from 'axios';
+import sendToTlg from '@/pages/api/sendToTlg';
 import { useState } from 'react';
 import Button from '../Button/Button';
 
@@ -24,22 +24,18 @@ function RegisterForm() {
     mode: 'all',
   });
 
-  const onSubmitHandler = (data: Idata) => {
-    const TELEGRAM_TOKEN = '5921706384:AAE8BqSi4TTNxh8yEp7nEOhBvIv4ejYBjVQ';
-    const TELEGRAM_CHAT_ID = '-1001866687565';
-    const URI_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
-    let message = `
-      <b> Ім'я: ${data.username}</b>
-      <b> Телефон: ${data.phone}</b>
-      <b> Комментар: ${data.comment}</b>
-    `;
-    axios.post(URI_API, {
-      chat_id: TELEGRAM_CHAT_ID,
-      parse_mode: 'html',
-      text: message,
-    });
-    reset();
-    setIsSending(true);
+  const onSubmitHandler = async (data: Idata) => {
+    try {
+      fetch('/api/sendToTlg', {
+        method: 'post',
+        body: JSON.stringify(data),
+      });
+      setIsSending(true);
+    } catch (error) {
+      console.log('oops');
+    } finally {
+      reset();
+    }
   };
 
   return isSending ? (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import DropDown from '@/components/DropDown/DropDown';
 import Layout from '@/components/Layout/Layout';
-import WorkForm from '@/components/WorkForm/WorkForm';
+import RegisterForm from '@/components/RegisterForm/RegisterForm';
 export default function Home() {
   return (
     <Layout>
@@ -8,7 +8,7 @@ export default function Home() {
         <section className="section">
           <div className="container bg-slate-700 font-display font-bold">
             <DropDown>
-              <WorkForm />
+              <RegisterForm />
             </DropDown>
           </div>
         </section>
