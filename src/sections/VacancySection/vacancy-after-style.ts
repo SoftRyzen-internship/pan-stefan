dc04f5818fc4357367bc09778db88675df1e91cb
Ann Sarviro <annka_nuha@ai.ua>
2023-06-01 18:40:41 +0300

    fix: update bd-style in VacancySection

--- src/sections/VacancySection/vacancy-after-style.ts
+++ src/sections/VacancySection/vacancy-after-style.ts
@@ -1,3 +1,5 @@
 const afterStyle = `xl:after:absolute xl:after:contents='' xl:after:w-[100%] xl:after:h-[100%] xl:after:block xl:after:left-2.5 xl:after:-top-2.5 xl:after:border xl:after:rounded-[400px] xl:after:border-lightBrown-30`;
+const beforeBgStyle = `before:bg-[url('./../../public/assets/images/vacancy/vacancybg1.png')] md:before:top-[-4.2rem] xl:before:top-[-2rem] xl:before:left-[calc(50%_+_286px)] md:before:left-[calc(50%_+_140px)] md:after:before='' md:before:absolute md:before:w-[392px] md:before:h-[300px] md:before:block max-xl:before:scale-[0.63]`;
+const afterBgStyle = `after:bg-[url('./../../public/assets/images/vacancy/vacancybg2.png')] md:after:bottom-[-10.2rem] xl:after:bottom-[-10rem] xl:after:right-[calc(50%_+_305px)] md:after:right-[calc(50%_+_96px)] md:after:contents='' md:after:absolute md:after:w-[461px] md:after:h-[445px] md:after:block max-xl:after:scale-[0.63]`;
 
-export default afterStyle;
+export default { afterStyle, beforeBgStyle, afterBgStyle };
