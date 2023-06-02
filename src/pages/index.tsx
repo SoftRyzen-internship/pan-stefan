2b1592767f282d1615fca569393b6ef3f1137b09
Ann Sarviro <annka_nuha@ai.ua>
2023-06-02 23:42:39 +0300

    feat: contact section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Layout from '@/components/Layout/Layout';
 import HeroSection from '@/sections/HeroSection/HeroSection';
 import AboutUsSection from '@/sections/AboutUsSection/AboutUsSection';
 import VacancySection from '@/sections/VacancySection/VacancySection';
+import ContactSection from '@/sections/ContactSection/ContactSection';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <HeroSection />
         <AboutUsSection />
         <VacancySection />
+        <ContactSection />
       </>
     </Layout>
   );
