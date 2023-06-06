c391b3ae57743d51c016ddcb087c11cf59060407
Ann Sarviro <annka_nuha@ai.ua>
2023-06-06 12:07:23 +0300

    fix: fix logo hover

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,9 +6,9 @@ import Logo from '@/public/assets/icon/LogoPan.svg';
 const CompanyLogo: FC = () => (
   <NextLink
     href="/"
-    className="block mx-auto hover:scale-125 md:hover:scale-100 text-center h-[40px] md:h-[60px] max-w-[56px] md:max-w-[85px] cursor-pointer text-purple-80 hover:text-purple-30"
+    className="block mx-auto hover:scale-125 md:hover:scale-100 text-center max-w-[56px] md:max-w-[85px] cursor-pointer text-purple-80 hover:text-purple-30 fill-purple-80 hover:fill-purple-30"
   >
-    <Logo aria-label="Company logo" className="mx-auto" />
+    <Logo aria-label="Company logo" className="mx-auto fill-inherit hover:inherit" />
     <p
       className={`mx-auto font-tanPearl text-[18px]/[16px] md:text-[26px]/[22px] uppercase max-w-[85px]`}
     >
