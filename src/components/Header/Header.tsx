7754ccf5b3d62c1f0880257fb50f65c875261514
Ann Sarviro <annka_nuha@ai.ua>
2023-06-06 14:16:33 +0300

    fix: fix header mobile menu container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, Transition } from '@headlessui/react';
 import { XMarkIcon, Bars3BottomLeftIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
 import { Link, scroller } from 'react-scroll';
@@ -25,11 +25,11 @@ function Header() {
 
   return (
     <header>
-      <div className="border-b border-lightBrown-30">
-        <Disclosure as="nav" className="container relative px-5 md:px-8 xl:px-8">
+      <div className="border-b border-lightBrown-30 relative ">
+        <Disclosure as="nav" className="container  px-5 md:px-8 xl:px-8">
           {({ open }) => (
             <>
-              <div className=" flex items-center justify-between h-[100px]">
+              <div className=" flex items-center justify-between py-5">
                 <div className="flex items-center">
                   <CompanyLogo />
                   <div className="hidden xl:block">
@@ -84,13 +84,13 @@ function Header() {
               </div>
 
               <Disclosure.Panel
-                className={`pb-[41px] pt-[60px] md:py-[100px] absolute hero-bg bg-center xl:hidden bg-lightWite left-0 top-full z-10 mt-[1px] w-screen ${
+                className={`pb-[41px] pt-[60px] md:py-[100px] absolute hero-bg bg-center xl:hidden bg-lightWite left-0 right-0 top-full z-10 mt-[1px] w-screen ${
                   open ? '' : 'hidden'
                 }`}
               >
                 {({ open, close }) => (
-                  <div className="md:px-8 flex flex-col items-center md:flex-row md:flex md:justify-between">
-                    <ul className="space-y-5 md:space-y-7 flex flex-col items-center md:items-start md:justify-center mb-[60px] ">
+                  <div className="container flex flex-col items-center md:flex-row md:flex md:justify-between">
+                    <ul className="space-y-5 md:space-y-[20px] flex flex-col items-center md:items-start md:justify-center mb-[60px] ">
                       {navigation.map(item => (
                         <li key={item.id}>
                           <Link
