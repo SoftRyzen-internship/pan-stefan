520d97ef1b125ee57f9a4aa0fe58606ba124ee6c
Mykola-Veres <veresnickolay@gmail.com>
2023-06-06 14:14:38 +0300

    add-hero2x

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -7,7 +7,7 @@ import Button from 'components/Button/Button';
 import useBreakpoints from 'services/hooks/useBreakpoints';
 
 import heroTextContent from './hero-text-content';
-import mainImage from '@/public/assets/images/hero/main-image.png';
+import mainImage from '@/public/assets/images/hero/Hero@2x.png';
 
 function HeroSection() {
   const { less768px, bigger1280px } = useBreakpoints();
