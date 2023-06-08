2caa67e3d3d5c563946a48addd045bf19c5882eb
Mykola-Veres <veresnickolay@gmail.com>
2023-06-08 17:04:14 +0300

    add-retina

--- src/components/Slider/sliderImages.ts
+++ src/components/Slider/sliderImages.ts
@@ -1,11 +1,12 @@
 const sliderImages = [
-  { id: '1', src: '/assets/images/slider/photo1.jpg', name: 'Тістечка з кремом' },
-  { id: '2', src: '/assets/images/slider/photo2.jpg', name: 'Тірамісу' },
-  { id: '3', src: '/assets/images/slider/photo3.jpg', name: 'Тарталетка' },
-  { id: '4', src: '/assets/images/slider/photo4.jpg', name: 'Тістечка макарони' },
-  { id: '5', src: '/assets/images/slider/photo5.jpg', name: 'Тістечка желейні' },
-  { id: '6', src: '/assets/images/slider/photo6.jpg', name: 'Тістечко з чашкою кави' },
-  { id: '7', src: '/assets/images/slider/photo7.jpg', name: 'Тістечко з кремом і фруктами' },
+  { id: '1', src: '/assets/images/slider/photo1@2x.jpg', name: 'Тістечка з кремом' },
+  { id: '2', src: '/assets/images/slider/photo2@2x.jpg', name: 'Тірамісу' },
+  { id: '3', src: '/assets/images/slider/photo3@2x.jpg', name: 'Тарталетка' },
+  { id: '4', src: '/assets/images/slider/photo4@2x.jpg', name: 'Тістечка макарони' },
+  { id: '5', src: '/assets/images/slider/photo5@2x.jpg', name: 'Тістечка желейні' },
+  { id: '6', src: '/assets/images/slider/photo6@2x.jpg', name: 'Тістечко з чашкою кави' },
+  { id: '7', src: '/assets/images/slider/photo7@2x.jpg', name: 'Жовте тістечко' },
+  { id: '8', src: '/assets/images/slider/photo8@2x.jpg', name: 'Тістечко з кремом і фруктами' },
 ];
 
 export default sliderImages;
