674c85b0ab06271eb308e94e8ebb7420eff47777
Ann Sarviro <annka_nuha@ai.ua>
2023-06-08 15:53:31 +0300

    fix:height product card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -8,8 +8,8 @@ function ProductCard(props: IProduct) {
   const { category, productDescription, src } = product;
 
   return (
-    <li className="max-md:mx-auto max-md:mb-[32px] relative px-5 md:px-8 bg-lightWite text-center pt-[214px] pb-5 md:pb-8 xl:pb-10 min-h-[508px] s:min-h-[462px] md:min-h-[472px] xl:min-h-[456px] w-[280px] s:w-[300px] md:w-[336px] xl:w-[384px] rounded-t-[200px] border border-purple-80">
-      <div className="absolute left-0 right-0 w-[100%] top-[-5%]">
+    <li className="md:place-self-stretch max-md:mx-auto max-md:mb-[32px] relative px-5 md:px-8 bg-lightWite text-center pt-[214px] pb-5 md:pb-8 xl:pb-10 min-h-[508px] s:min-h-[462px] md:min-h-[472px] xl:min-h-[456px] w-[280px] s:w-[300px] md:w-[336px] xl:w-[384px] rounded-t-[200px] border border-purple-80">
+      <div className="absolute left-0 right-0 w-[100%] top-[-5%] xl:top-[-8%]">
         {
           <Image
             width={240}
@@ -24,9 +24,7 @@ function ProductCard(props: IProduct) {
       <h3 className="relative block font-cormorant text-purple-80 font-semibold text-[32px] mb-[22px] leading-9	w-[100%] after:absolute  after:bg-cardGradient after:w-[162px] after:h-[1px] after:bottom-[-10px] after:left-[50%] after:translate-x-[-50%] after:block">
         {category}
       </h3>
-      <div className="overflow-hidden h-[192px] md:h-[168px] xl:h-[144px]">
-        <p className="text-base text-black">{productDescription}</p>
-      </div>
+      <p className="text-base text-black">{productDescription}</p>
     </li>
   );
 }
