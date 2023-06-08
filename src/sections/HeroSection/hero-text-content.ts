ee94951ada19a4b6bd483a3a65dd7a4194c3c52f
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-06-08 14:35:43 +0300

    Fix grrammatic and change AboutUs image

--- src/sections/HeroSection/hero-text-content.ts
+++ src/sections/HeroSection/hero-text-content.ts
@@ -2,7 +2,7 @@ const heroTextContent = {
     logoText: 'Pan Stefan',
     descriptionText: 'Наповнюємо ваш кожен день маленькою радістю, наші смаколики - то любов',
     buttonText: 'Продукція',
-    imageAltText: 'Святковий пиріг зі свечами',
+    imageAltText: 'Святковий пиріг зі свічками',
 };
 
 export default heroTextContent;
