ff39e4fa138d8291a3fbf8a0e1ce6547cc5c9e95
KORZH ILLIA <kiev_drum2006@ukr.net>
2023-06-08 13:09:49 +0300

    Update Buttons styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,11 +6,11 @@ function Button(props: ButtonProps) {
     <button
       type={type}
       onClick={onClick}
-      className={`${centered ? 'mx-auto' : ''} ${long ? 'w-[240px]' : ''} ${
+      className={`${centered ? 'mx-auto' : ''} ${long ? 'w-[240px]' : 'max-w-[240px]'} ${
         lightHover
           ? 'hover:bg-lightWite hover:text-purple-80 focus:bg-lightWite focus:text-purple-80'
           : 'hover:bg-purple-30 focus:bg-purple-30'
-      } min-w-[178px] border-0 flex justify-center items-center py-[14px] bg-purple-80 transition-all rounded-full font-normal text-white text-base leading-[1.5]  ${
+      } min-w-[178px] border-0 flex justify-center items-center py-[14px] px-[12px] bg-purple-80 transition-all rounded-full font-normal text-white text-base leading-[1.5]  ${
         xwide ? 'w-[100%]' : ''
       }`}
     >
