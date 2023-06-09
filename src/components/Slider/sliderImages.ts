10cb1a9068d97594a2ffd2675a7e87003899bbd8
Ann Sarviro <annka_nuha@ai.ua>
2023-06-09 15:18:59 +0300

    fix: gsllery alt text

--- src/components/Slider/sliderImages.ts
+++ src/components/Slider/sliderImages.ts
@@ -2,7 +2,7 @@ const sliderImages = [
   { id: '1', src: '/assets/images/slider/photo1@2x.jpg', name: 'Тістечка з кремом' },
   { id: '2', src: '/assets/images/slider/photo2@2x.jpg', name: 'Тірамісу' },
   { id: '3', src: '/assets/images/slider/photo3@2x.jpg', name: 'Тарталетка' },
-  { id: '4', src: '/assets/images/slider/photo4@2x.jpg', name: 'Тістечка макарони' },
+  { id: '4', src: '/assets/images/slider/photo4@2x.jpg', name: 'Тістечка макарунс' },
   { id: '5', src: '/assets/images/slider/photo5@2x.jpg', name: 'Тістечка желейні' },
   { id: '6', src: '/assets/images/slider/photo6@2x.jpg', name: 'Тістечко з чашкою кави' },
   { id: '7', src: '/assets/images/slider/photo7@2x.jpg', name: 'Жовте тістечко' },
