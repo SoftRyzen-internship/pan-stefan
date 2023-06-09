96ca6251f549f09e4c5197ba74034ad93eea7a9f
Ann Sarviro <annka_nuha@ai.ua>
2023-06-09 10:11:52 +0300

    fix:footerscale

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import CompanyLogo from '../../components/Logo/Logo';
 
 const Footer = () => {
   return (
-    <footer className="container max-md:hover:scale-125 text-center py-[32px] md:py-[40px] xl:py-[60px]">
+    <footer className="container text-center py-[32px] md:py-[40px] xl:py-[60px]">
       <CompanyLogo />
     </footer>
   );
