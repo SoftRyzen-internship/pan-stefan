014463f23b7a7ffda27b1f5f238b4b6b6fc04865
Mykola-Veres <veresnickolay@gmail.com>
2023-06-09 15:03:28 +0300

    add-logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,23 +1,35 @@
-import { FC } from 'react';
 import NextLink from 'next/link';
 
 import Logo from '@/public/assets/icon/logo.svg';
+import useBreakpoints from 'services/hooks/useBreakpoints';
+import LogoM from '@/public/assets/icon/logoM.svg';
 
-const CompanyLogo: FC = () => (
-  <NextLink
-    href="/"
-    className="block mx-auto hover:scale-125 md:hover:scale-100 text-center max-w-[56px] md:max-w-[85px] cursor-pointer text-purple-80 hover:text-purple-30 focus:text-purple-30 fill-purple-80 hover:fill-purple-30 focus:fill-purple-30"
-  >
-    <Logo
-      aria-label="Логотип компанії"
-      className="mx-auto fill-inherit hover:inherit focus:inherit"
-    />
-    <p
-      className={`mx-auto font-tanPearl text-lg leading-[0.8] md:text-[26px] uppercase max-w-[85px]`}
+function CompanyLogo() {
+  const { less768px } = useBreakpoints();
+
+  return (
+    <NextLink
+      href="/"
+      className="md:h-[85px] block mx-auto hover:scale-125 md:hover:scale-100 text-center max-w-[56px] md:max-w-[85px] cursor-pointer text-purple-80 hover:text-purple-30 focus:text-purple-30 fill-purple-80 hover:fill-purple-30 focus:fill-purple-30"
     >
-      Pan Stefan
-    </p>
-  </NextLink>
-);
+      {less768px ? (
+        <LogoM
+          aria-label="Логотип компанії"
+          className="mx-auto fill-inherit hover:inherit focus:inherit"
+        />
+      ) : (
+        <Logo
+          aria-label="Логотип компанії"
+          className="mx-auto fill-inherit hover:inherit focus:inherit"
+        />
+      )}
+      <p
+        className={`mx-auto font-tanPearl text-[12px] md:text-[24px] md:leading-8 uppercase max-w-[44px] md:max-w-[85px]`}
+      >
+        Pan Stefan
+      </p>
+    </NextLink>
+  );
+}
 
 export default CompanyLogo;
